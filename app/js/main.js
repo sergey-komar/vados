b607621f409d2d2795c3ed284208d1e91093c4ce
sergey-komar <kursev81@yandex.ru>
2022-06-28 23:07:33 +0300

    menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function () {
     var mixer = mixitup('.products__inner-box');
-})
+
+    $('.rate-star').rateYo({
+        rating: 3.6,
+        starWidth: "12px"
+      });
+
+    $('.product-slider__inner').slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 4,
+    slidesToScroll: 3,
+    })
+let test = document.querySelectorAll('.product__item');
+
+
+
+  test.classList.add('test');
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,8 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
